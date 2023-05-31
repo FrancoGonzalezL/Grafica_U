a76ca9dc17f07ac4606ac81f53ba62d2043c0c78
FrancoGonzalezL <frango.gonzalez.l@ug.uchile.cl>
2023-05-30 22:41:01 -0400

    tarea3

--- Tareas/Tarea3/objetos.py
+++ Tareas/Tarea3/objetos.py
@@ -1,6 +1,11 @@
 import libs.transformations as tr
 import numpy as np
 from libs.scene_graph import findNode
+from libs.basic_shapes import createTextureNormalsCube
+from libs.assets_path import getAssetPath
+from libs.gpu_shape import createGPUShape
+from libs.shaders import textureSimpleSetup
+from OpenGL.GL import *
 
 class Camera:
     def __init__(self,controller, width , height,):
@@ -65,27 +70,42 @@ class Nave:
                                       tr.rotationY(np.pi/2), tr.uniformScale(0.2)])
 
 class Ruta:
-    def __init__(self):
+    def __init__(self,pipeline):
         self.ruta = []
         self.tiempo = []
         self.direccion = []
 
-    def iniciar_grabacion(self,posicion,tiempo,direccion):
-        self.ruta = [posicion]
-        self.tiempo = [tiempo]
-        self.direccion = [direccion]
+        self.cubo = createGPUShape(pipeline, createTextureNormalsCube(1,1,1,1))
+        self.cubo.texture = textureSimpleSetup(getAssetPath("RED.png"), GL_REPEAT, GL_REPEAT, GL_NEAREST, GL_NEAREST)
+        self.grabar = False
+        self.show   = False
 
-    def actualizar(self,posicion,tiempo,direccion):
-        self.ruta.append(posicion)
-        self.tiempo.append(tiempo)
-        self.direccion.append(direccion)
+    def iniciar_grabacion(self,nave,time):
+        self.grabar = True
+        self.ruta = [[nave.positionX,nave.positionY,nave.positionZ]]
+        self.tiempo = [time]
+        self.direccion = [[nave.theta,nave.phi]]
+        self.print()
+
+    def actualizar(self,nave,time):
+        if self.grabar:
+            self.ruta.append([nave.positionX,nave.positionY,nave.positionZ])
+            self.tiempo.append(time)
+            self.direccion.append([nave.theta,nave.phi])
+            self.print()
 
     def print(self):
         if len(self.ruta)!=0:
             print("punto agregado: ",self.ruta[-1],end=", ")
             print("orientacion:", self.direccion[-1],end=", ")
             print("total de puntos: ", len(self.ruta))
-        
+    def draw(self,pipeline):
+        if self.show:
+            for posicion in self.ruta:
+                model = tr.matmul([tr.translate(posicion[0],posicion[1],posicion[2]),
+                                tr.uniformScale(0.2)])
+                glUniformMatrix4fv(glGetUniformLocation(pipeline.shaderProgram, "model"), 1, GL_TRUE, model)
+                pipeline.drawCall(self.cubo)
 
 #Este Grupo solo da vueltas
 class Obstaculos:
--- Tareas/Tarea3/tarea3.py
+++ Tareas/Tarea3/tarea3.py
@@ -20,7 +20,6 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
     A,D: para girar (izquierda, derecha)
     mouse: la nave sube o baja dependiendo de la posicion del mouse
 """
-
 #se puede modificar
 WIDTH, HEIGHT = 1100, 950
 
@@ -40,11 +39,9 @@ class Controller(pyglet.window.Window):
         #mapa
         self.anchoMapa = 16
         self.largoMapa = 30
-        
         #nave
         self.nave_speed         = 3.3
         self.nave_angular_speed = 3.3
-
         #OBJETOS
         #muros
         self.muros_densidad   = 0.09
@@ -53,53 +50,59 @@ class Controller(pyglet.window.Window):
         #meteoritos
         self.meteoritos_total = 2
         #------------------------
-
 controller = Controller(width=WIDTH, height=HEIGHT)
+
 
 camera = objetos.Camera(controller, WIDTH, HEIGHT)
 nave = objetos.Nave(controller.nave_speed, controller.nave_angular_speed)
-ruta = objetos.Ruta()
-
+ruta = objetos.Ruta(controller.pipeline)
 muros = objetos.MurosMapa(controller, controller.muros_densidad, controller.muros_altura_max, controller.muros_largo_max)
 meteoritos = objetos.Meteoritos(controller, "meteorito", controller.meteoritos_total)
 obstaculos = np.array([objetos.Obstaculos(controller,"among1", 0.5),
                        objetos.Obstaculos(controller,"pochita", 2.0),
                        objetos.Obstaculos(controller,"among2", 0.6)])
-
 escena = grafo.grafo(controller,controller.pipeline, muros, meteoritos)
+
 
 glUseProgram(controller.pipeline.shaderProgram)
 glClearColor(0.0, 0.7, 1.0, 1.0)
 glEnable(GL_DEPTH_TEST)
+
+glUniform3f(glGetUniformLocation(controller.pipeline.shaderProgram, "La"), 1.0, 1.0, 1.0)
+glUniform3f(glGetUniformLocation(controller.pipeline.shaderProgram, "Ld"), 1.0, 1.0, 1.0)
+glUniform3f(glGetUniformLocation(controller.pipeline.shaderProgram, "Ls"), 1.0, 1.0, 1.0)
+glUniform3f(glGetUniformLocation(controller.pipeline.shaderProgram, "Ka"), 0.35, 0.35, 0.35)
+glUniform3f(glGetUniformLocation(controller.pipeline.shaderProgram, "Kd"), 0.9, 0.9, 0.9)
+glUniform3f(glGetUniformLocation(controller.pipeline.shaderProgram, "Ks"), 1.0, 1.0, 1.0)
+glUniform1f(glGetUniformLocation(controller.pipeline.shaderProgram, "constantAttenuation"), 0.0001)
+glUniform1f(glGetUniformLocation(controller.pipeline.shaderProgram, "linearAttenuation"), 0.03)
+glUniform1f(glGetUniformLocation(controller.pipeline.shaderProgram, "quadraticAttenuation"), 0.01)
+glUniform1ui(glGetUniformLocation(controller.pipeline.shaderProgram, "shininess"), 100)
+
 
 @controller.event
 def on_key_press(symbol, modifiers):
     if symbol == pyglet.window.key.ESCAPE:
         controller.close()
 
+    ruta.actualizar(nave,controller.total_time)
     if symbol == pyglet.window.key.W:
         nave.speed =  1
-        ruta.actualizar([nave.positionX,nave.positionY,nave.positionZ],controller.total_time,[nave.theta])
-        ruta.print()
     elif symbol == pyglet.window.key.S:
         nave.speed = -1
-        ruta.actualizar([nave.positionX,nave.positionY,nave.positionZ],controller.total_time,[nave.theta])
-        ruta.print()
     elif symbol == pyglet.window.key.A:
         nave.angular_speed = -1
-        ruta.actualizar([nave.positionX,nave.positionY,nave.positionZ],controller.total_time,[nave.theta])
-        ruta.print()
     elif symbol == pyglet.window.key.D:
         nave.angular_speed =  1
-        ruta.actualizar([nave.positionX,nave.positionY,nave.positionZ],controller.total_time,[nave.theta])
-        ruta.print()
 
     if symbol == pyglet.window.key.R:
-        ruta.iniciar_grabacion([nave.positionX,nave.positionY,nave.positionZ],controller.total_time,[nave.theta])
-        ruta.print()
+        ruta.iniciar_grabacion(nave,controller.total_time)
+    if symbol == pyglet.window.key.V:
+        ruta.show = not ruta.show
 
 @controller.event
 def on_key_release(symbol, modifiers):
+    ruta.actualizar(nave,controller.total_time)
     if   symbol == pyglet.window.key.W:
         nave.speed = 0
     elif symbol == pyglet.window.key.S:
@@ -120,36 +123,24 @@ def on_draw():
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
     
     camera.update(controller, nave)
-    glUniform3f(glGetUniformLocation(controller.pipeline.shaderProgram, "La"), 1.0, 1.0, 1.0)
-    glUniform3f(glGetUniformLocation(controller.pipeline.shaderProgram, "Ld"), 1.0, 1.0, 1.0)
-    glUniform3f(glGetUniformLocation(controller.pipeline.shaderProgram, "Ls"), 1.0, 1.0, 1.0)
-    glUniform3f(glGetUniformLocation(controller.pipeline.shaderProgram, "Ka"), 0.35, 0.35, 0.35)
-    glUniform3f(glGetUniformLocation(controller.pipeline.shaderProgram, "Kd"), 0.9, 0.9, 0.9)
-    glUniform3f(glGetUniformLocation(controller.pipeline.shaderProgram, "Ks"), 1.0, 1.0, 1.0)
 
     glUniform3f(glGetUniformLocation(controller.pipeline.shaderProgram, "lightPosition"), nave.positionX, nave.positionY + 20, nave.positionZ)
     glUniform3f(glGetUniformLocation(controller.pipeline.shaderProgram, "viewPosition"),  camera.eye[0], camera.eye[1], camera.eye[2])
-    glUniform1ui(glGetUniformLocation(controller.pipeline.shaderProgram, "shininess"), 100)
-    
-    glUniform1f(glGetUniformLocation(controller.pipeline.shaderProgram, "constantAttenuation"), 0.0001)
-    glUniform1f(glGetUniformLocation(controller.pipeline.shaderProgram, "linearAttenuation"), 0.03)
-    glUniform1f(glGetUniformLocation(controller.pipeline.shaderProgram, "quadraticAttenuation"), 0.01)
-
-
     glUniformMatrix4fv(glGetUniformLocation(controller.pipeline.shaderProgram, "projection"), 1, GL_TRUE, camera.projection)
     glUniformMatrix4fv(glGetUniformLocation(controller.pipeline.shaderProgram, "view"), 1, GL_TRUE, camera.view)
-    glUniformMatrix4fv(glGetUniformLocation(controller.pipeline.shaderProgram, "model"), 1, GL_TRUE, tr.identity())
+
     sg.drawSceneGraphNode(escena, controller.pipeline, "model")
+    ruta.draw(controller.pipeline)
 
-
-def update(dt,controller, nave, obstaculos, meteoritos, escena):
+def update(dt,controller, nave, obstaculos, meteoritos, escena, ruta):
     controller.total_time += dt
     nave.update(controller, escena, dt)
     meteoritos.update(controller,escena,dt)
     for obstaculo in obstaculos:
         obstaculo.update(controller,escena, dt)
-    
+    if ruta.grabar and ((controller.total_time - ruta.tiempo[-1]) > 1):
+        ruta.actualizar(nave,controller.total_time)
 
 if __name__ == '__main__':
-    pyglet.clock.schedule(update, controller, nave, obstaculos,meteoritos, escena)
+    pyglet.clock.schedule(update, controller, nave, obstaculos,meteoritos, escena, ruta)
     pyglet.app.run(1/60)
